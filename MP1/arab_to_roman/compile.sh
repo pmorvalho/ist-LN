e50d9353daf1f76321436d88922a824be7728107
pmorvalho <pmorvalho@gmail.com>
2017-10-05 20:24:28 +0100

    Conversion from arabic to roman numerals done

--- MP1/arab_to_roman/compile.sh
+++ MP1/arab_to_roman/compile.sh
@@ -1,36 +1,40 @@
 #!/bin/bash
 
-rm *.fst *.png *transdutorRomano.*
+# rm *.fst *.png *transdutorRomano.*
 ################### Tradutores de tradução ################
 # Compilar e desenhar transdutores parciais
-for f in startwith[1-9].txt
-do
-	filename=$(basename $f .txt) # $f -> startwith1.txt
-	echo "compiling number $filename" # $filename -> startwith1
-	fstcompile --isymbols=syms.txt --osymbols=syms.txt $f | fstarcsort > $filename.fst
-	fstminimize $filename.fst $filename.fst
-	fstarcsort $filename.fst $filename.fst
-	fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait $filename.fst | dot -Tpng  > $filename.png
-done
-
+# for f in startwith[1-9].txt
+# do
+# 	filename=$(basename $f .txt) # $f -> startwith1.txt
+# 	echo "compiling number $filename" # $filename -> startwith1
+# 	fstcompile --isymbols=syms.txt --osymbols=syms.txt $f | fstarcsort > $filename.fst
+# 	fstminimize $filename.fst $filename.fst
+# 	fstarcsort $filename.fst $filename.fst
+# 	fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait $filename.fst | dot -Tpng  > $filename.png
+# done
+fstcompile --isymbols=syms.txt --osymbols=syms.txt roman2arab.txt | fstarcsort > roman2arab.fst
+fstminimize roman2arab.fst roman2arab.fst
+fstarcsort $filename.fst $filename.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait roman2arab.fst | dot -Tpng  > roman2arab.png
 
 # Juntar os transdutores parciais num geral
-fstunion startwith1.fst startwith2.fst transdutorRomano.fst
-fstunion transdutorRomano.fst startwith3.fst transdutorRomano.fst
-fstunion transdutorRomano.fst startwith4.fst transdutorRomano.fst
-fstunion transdutorRomano.fst startwith5.fst transdutorRomano.fst
-fstunion transdutorRomano.fst startwith6.fst transdutorRomano.fst
-fstunion transdutorRomano.fst startwith7.fst transdutorRomano.fst
-fstunion transdutorRomano.fst startwith8.fst transdutorRomano.fst
-fstunion transdutorRomano.fst startwith9.fst transdutorRomano.fst
-fstrmepsilon transdutorRomano.fst transdutorRomano.fst
-fstarcsort transdutorRomano.fst transdutorRomano.fst
-fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait transdutorRomano.fst | dot -Tpng  > transdutorRomano.png
+# fstunion startwith1.fst startwith2.fst transdutorRomano.fst
+# fstunion transdutorRomano.fst startwith3.fst transdutorRomano.fst
+# fstunion transdutorRomano.fst startwith4.fst transdutorRomano.fst
+# fstunion transdutorRomano.fst startwith5.fst transdutorRomano.fst
+# fstunion transdutorRomano.fst startwith6.fst transdutorRomano.fst
+# fstunion transdutorRomano.fst startwith7.fst transdutorRomano.fst
+# fstunion transdutorRomano.fst startwith8.fst transdutorRomano.fst
+# fstunion transdutorRomano.fst startwith9.fst transdutorRomano.fst
+fstrmepsilon roman2arab.fst roman2arab.fst
+fstarcsort roman2arab.fst roman2arab.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait roman2arab.fst | dot -Tpng  > roman2arab.png
 
 
 # Versao minimizada do transdutor final
-fstdeterminize transdutorRomano.fst mintransdutorRomano.fst
-fstminimize mintransdutorRomano.fst mintransdutorRomano.fst
-fstarcsort mintransdutorRomano.fst mintransdutorRomano.fst
-fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait mintransdutorRomano.fst | dot -Tpng  > mintransdutorRomano.png
+# fstdeterminize roman2arab.fst mintransdutorRomano.fst
+# fstminimize mintransdutorRomano.fst mintransdutorRomano.fst
+fstinvert	roman2arab.fst arab2roman.fst
+fstarcsort arab2roman.fst arab2roman.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait arab2roman.fst | dot -Tpng  > arab2roman.png
 
--- MP1/arab_to_roman/tests/runtests.sh
+++ MP1/arab_to_roman/tests/runtests.sh
@@ -1,12 +1,26 @@
 ################### Testa os tradutores ################
 rm  *.fst *.pdf *.png *.ps
 
+rm -r fst_out
+rm -r fst_in
+
+mkdir fst_in
+mkdir fst_out
+mkdir numbers
+
+python gennumbers.py
+
 for f in numbers/*.txt
 do
 	filename=$(basename $f .txt)
 	echo "testing number $filename"
 	fstcompile --isymbols=syms.txt --osymbols=syms.txt $f | fstarcsort > fst_in/$filename.fst
-	fstcompose fst_in/$filename.fst ../transdutorRomano.fst > fst_out/test_$filename.fst
-	#fstcompose fst_in/$filename.fst ../mintransdutorRomano.fst > fst_out/test_$filename.fst
+	fstcompose fst_in/$filename.fst ../arab2roman.fst > fst_out/test_$filename.fst
+	#fstcompose fst_in/$filename.fst ../minarab2roman.fst > fst_out/test_$filename.fst
 	fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait fst_out/test_$filename.fst | dot -Tpng > test_$filename.png
 done
+
+rm  *.fst *.pdf *.png *.ps
+rm -r fst_out
+rm -r fst_in
+rm -r numbers
