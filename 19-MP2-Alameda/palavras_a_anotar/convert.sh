25abbc60150bebd5be70dc17945fe2d29dfa5b9f
Daniel Correia <dduarte96@hotmail.com>
2017-11-03 18:24:10 +0000

    updated ngrams counts calculation and added sorted bigrams output

--- 19-MP2-Alameda/palavras_a_anotar/convert.sh
+++ 19-MP2-Alameda/palavras_a_anotar/convert.sh
@@ -9,3 +9,7 @@ cp */*.out .
 # gerar ficheiros dos unigrams e bigramas
 ./calculate_ngrams.py vimosVerVir.final vimos
 ./calculate_ngrams.py cobreCobrarCobrir.final cobre
+
+# ordernar bigramas por ordem alfabetica (opcional)
+cat bigrams_cobre.txt | sort > bigrams_sorted_cobre.txt
+cat bigrams_vimos.txt | sort > bigrams_sorted_vimos.txt
--- 19-MP2-Alameda/palavras_a_anotar/convert_out_to_final.py
+++ 19-MP2-Alameda/palavras_a_anotar/convert_out_to_final.py
@@ -2,20 +2,19 @@
 
 
 import sys
+import re
 
-
-def clean_input_file(filename,word):
+def clean_input_file(filename, word):
 	output_file = filename.split(".out")[0] + ".final"
-	open(output_file,"w").close() # reset output file
-	with open(filename,"r") as f, open(output_file,"a") as out:
-		for line in f:
-			label, frase  = line.split("\t")
-			if not any(x in label for x in ["#","?","n-é-verbo"]):
-				if frase.lower().count(word) == 1:
-					out.write(frase.replace(word,label))
+	open(output_file, "w").close() # reset output file
+	with open(filename, "r") as read_f, open(output_file, "a") as out:
+		for line in read_f:
+			label, frase = line.split("\t")
+			if not any(x in label for x in ["#", "?", "n-é-verbo"]):
+				re_pat = r'\b' + word + r'\b'
+				if len(re.findall(re_pat, frase.lower())) == 1:
+					out.write(re.sub(re_pat,label,frase))
 
 
 if __name__ == '__main__':
-	filename = sys.argv[1]
-	word = sys.argv[2]
-	clean_input_file(filename,word)
+	clean_input_file(sys.argv[1], sys.argv[2])
