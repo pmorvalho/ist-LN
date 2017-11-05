6847fbf1a65aa88a8ce19d56a22a8e5b1303a1e1
Daniel Correia <dduarte96@hotmail.com>
2017-11-05 13:21:57 +0000

    Small fixes 1. show unsmoothed results 2. fix tokenization of phrases 3. reduce complexity of reading input from files

--- 19-MP2-Alameda/palavras_a_anotar/lemmaDealer.py
+++ 19-MP2-Alameda/palavras_a_anotar/lemmaDealer.py
@@ -6,18 +6,21 @@
 
 import sys
 import re
+from nltk import word_tokenize
 
 global unigrams, bigrams, params, phrases, V
 
 def calculate_prob(phrase, word, lemmas):
-	words = phrase.split(" ")
-	results = []
+	words = word_tokenize(phrase)
+	smooth_results = []
+	basic_results = []
 	w_bef = w_after = ""
 	count_w_bef = 0
+
 	for i in range(len(words)):
 		if words[i] == word:
 			if i<len(words)-1:
-				w_after=wordss[i+1]
+				w_after=words[i+1]
 				break
 		elif i < len(words)-1:
 			if words[i+1]==word:
@@ -46,38 +49,50 @@ def calculate_prob(phrase, word, lemmas):
 			elif bi[1] == l and bi[0] == w_bef:
 				bi_before  = int(b[1])
 
-		results.append((l, (((bi_before+1)/(count_w_bef+V))*((bi_after+1)/(count_lemma+V)))))
+		smooth_results.append((l, (((bi_before+1)/(count_w_bef+V))*((bi_after+1)/(count_lemma+V)))))
+		try:
+			basic_results.append( (l, (bi_before/count_w_bef)*(bi_after/count_lemma) ) )
+		except ZeroDivisionError:
+			basic_results.append( (l, -1))
 
+	print("\n===================== SEM ALISAMENTO ============================")
 	maximum = (lemmas[0],0)
-	for r in results:
+	for r in basic_results:
 		if maximum[1] < r[1]:
 			maximum = r
 
-	print(str(phrase) + " -----> lema: " + maximum[0])
-	for r in results:
-		print("lema: " + r[0] + " probabilidade: " + str(r[1]))
+	print(str(phrase), " -----> lema: ", maximum[0],sep="")
+	for r in basic_results:
+		print("lema: ",r[0], "   probabilidade: ", r[1],sep="")
 
-	print()
+	print("===================== COM ALISAMENTO ============================")
+	maximum = (lemmas[0],0)
+	for r in smooth_results:
+		if maximum[1] < r[1]:
+			maximum = r
+
+	print(str(phrase), " -----> lema: ", maximum[0],sep="")
+	for r in smooth_results:
+		print("lema: ",r[0], "   probabilidade: ", r[1],sep="")
+
+
 if __name__ == '__main__':
 	if len(sys.argv)!=5:
 		print("Err, four files needed: Unigrams, Bigrams, Parameterization and Examples")
 	
 	try:
-		unigrams = (open(sys.argv[1], "r+").read()).split("\n")
-		V = len(unigrams)
-		for u in range(len(unigrams)):
-			unigrams[u]=unigrams[u].split("\t")
-		unigrams = unigrams[:-1]
+		unigrams = open(sys.argv[1], "r+").readlines()
+		unigrams = [line.strip().split("\t") for line in unigrams]
 
-		bigrams  = open(sys.argv[2], "r+").read().split("\n")
-		for b in range(len(bigrams)):
-			bigrams[b] = bigrams[b].split("\t")
-		bigrams = bigrams[:-1]
-		params   = (open(sys.argv[3], "r+").read()).split("\n")
+		V = len(unigrams)
+
+		bigrams = open(sys.argv[2], "r+").readlines()
+		bigrams = [line.strip().split("\t") for line in bigrams]
+
+		params   = open(sys.argv[3], "r+").read().strip().split("\n")
 		params[1] = params[1].split(" ")
-		phrases  = open(sys.argv[4], "r+").read().split("\n")
-		if phrases[-1:] == [""]:
-			phrases = phrases[:-1]
+
+		phrases  = open(sys.argv[4], "r+").read().strip().split("\n")
 
 	except:
 		print("Wrong format in files")
