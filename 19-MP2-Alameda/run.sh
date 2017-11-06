5ac13e8c81438d0eb50dde505c1727f3e00df50d
pmorvalho <pmorvalho@gmail.com>
2017-11-06 02:16:05 +0000

    Second delivery - first zip

--- 19-MP2-Alameda/run.sh
+++ 19-MP2-Alameda/run.sh
@@ -4,8 +4,6 @@
 # 80967, Daniel Correia
 # 81151, Pedro Orvalho
 
-cp */*.out .
-
 # converter ficheiros .out para .final
 ./convert_out_to_final.py vimosAnotado.out vimos
 ./convert_out_to_final.py cobreAnotado.out cobre
@@ -23,6 +21,9 @@ mv vimosBigramas-sorted.txt vimosBigramas.txt
 
 ./lemmaDealer.py vimosUnigramas.txt vimosBigramas.txt vimosParametrizacao.txt vimosFrases.txt
 
-echo "\n\n\n\n"
+echo ""
+echo ""
+echo ""
+echo ""
 
 ./lemmaDealer.py cobreUnigramas.txt cobreBigramas.txt cobreParametrizacao.txt cobreFrases.txt
